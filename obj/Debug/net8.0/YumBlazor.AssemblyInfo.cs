7cd15eb68a2378ac8b199f8f81a625a2b8651420
InnerChoseEvgenia <evgenialukashen@gmail.com>
2025-04-13 15:24:39 +0300

    Start Blazor App

--- obj/Debug/net8.0/YumBlazor.AssemblyInfo.cs
+++ obj/Debug/net8.0/YumBlazor.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("YumBlazor")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+986a8b50899fc08687fc7107b1ad6b178b67f0c0")]
 [assembly: System.Reflection.AssemblyProductAttribute("YumBlazor")]
 [assembly: System.Reflection.AssemblyTitleAttribute("YumBlazor")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
